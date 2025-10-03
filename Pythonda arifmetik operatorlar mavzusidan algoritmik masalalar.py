b5135686a273de3cdc2dc31288eec98530f63492
Farhod <zxcvbnmqazwsx287@gmail.com>
2025-10-03 16:32:11 +0500

    Create Pythonda arifmetik operatorlar mavzusidan algoritmik masalalar.py

---
+++ Pythonda arifmetik operatorlar mavzusidan algoritmik masalalar.py
@@ -0,0 +1,63 @@
+##Pythonda arifmetik operatorlar mavzusidan algoritmik masalalar
+##1
+# a = input("Sonni kiriting: ")
+# b = input("Sonni kiriting: ")
+# summa = a + b
+#
+# print(f"Yig'indi: {summa}")
+
+
+##2
+# l = int(input("Uchburchakning uzunligini kiriting: "))
+# w = int(input("Uchburchakning enini kiriting: "))
+# area = l * w
+# print(f"Uchburchak yuzasi: {area}")
+
+
+##3
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# avarage = (a + b + c) / 2
+# print(f"O'rta arifmetik: {avarage}")
+
+
+##4
+# a = int(input("Sonni kiriting: "))
+# b = int(input("Sonni kiriting: "))
+# perimetr = 2 * (a + b)
+# print(f"To‘g‘ri to‘rtburchak perimetri: {perimetr}")
+
+
+##5
+# h = int(input("Soatni kiriting: "))
+# m = int(input("Daqiqani kiriting: "))
+# total_min = h * 60 + m
+# print(f"Umumiy daqiqalar: {total_min}")
+
+
+##6
+# usd = int(input("USD miqdorini kiritng: "))
+# sum = usd * 12000
+# print(f"{sum}so'm")
+
+
+##7
+# a = int(input("Sonni kiriting: "))
+# b = int(input("Sonni kiriting: "))
+# diff = abs(a - b)
+# print(f"Diff = {diff}")
+
+
+##8
+# x = int(input("X'ni kiriting: "))
+# n = int(input("N'ni kiriting: "))
+# power = x**n
+# print(f"X'ning N - darajasi: {power}")
+
+
+##9
+# a = int(input("Sonni kiriting: "))
+# b = int(input("Sonni kiriting: "))
+# quotient = a // b
+# print(f"Bo‘linma va qoldiq = {quotient}")
