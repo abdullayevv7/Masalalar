ca51c4d23a0b1bcf6399394c3a7c639654faa28f
Farhod <zxcvbnmqazwsx287@gmail.com>
2025-10-03 15:38:43 +0500

    Create 03.10.2025(Masala).py

---
+++ 03.10.2025(Masala).py
@@ -0,0 +1,11 @@
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+#
+# print(f"Yig'indi: {a + b}")
+
+
+# familiya = input("Familiyangizni kiriting: ")
+# ism = input("Ismingizni kiriting: ")
+# otasining_ismi = input("Sharifingizni kiriting: ")
+#
+# print(f"{familiya} {ism} {otasining_ismi}")
